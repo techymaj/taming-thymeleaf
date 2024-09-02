232ffc1d87044c34e60bc4f5829a107d5ebd6cf3
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-09-03 01:35:10 +0300

    first commit

---
+++ src/main/java/org/example/tamingthymeleaf/TamingthymeleafApplication.java
@@ -0,0 +1,13 @@
+package org.example.tamingthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamingthymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamingthymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/tamingthymeleaf/TamingthymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tamingthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TamingthymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
