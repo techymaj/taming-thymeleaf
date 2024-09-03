01e3d9fd5c4b073fe90fa39ff18a4b3709c84bd0
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-09-03 23:59:49 +0300

    Writing our first template

--- src/main/java/org/example/tamingthymeleaf/TamingthymeleafApplication.java
+++ src/main/java/org/example/tamingthymeleaf/TamingthymeleafApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TamingthymeleafApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TamingthymeleafApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TamingthymeleafApplication.class, args);
+    }
 }
